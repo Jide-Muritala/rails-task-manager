4688db17ca9f711f486d07ae57912b730ba9ad7e
Babajide Muritala <jide142000@yahoo.com>
2023-02-01 15:53:16 +0100

    CREATE crud actions for get and post added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
 end
