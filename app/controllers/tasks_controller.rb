c3a9317f1a665f151ab17be2b6aafc725f5b9e55
Babajide Muritala <jide142000@yahoo.com>
2023-02-01 17:57:33 +0100

    UPDATE crud actions for get and patch added to app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,16 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
