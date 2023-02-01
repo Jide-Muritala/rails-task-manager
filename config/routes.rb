4516a4e7d86faec0ef6e7ceec8f949cd7f8cd505
Babajide Muritala <jide142000@yahoo.com>
2023-02-01 20:58:24 +0100

    DELETE crud action added to app

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit"
   patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
