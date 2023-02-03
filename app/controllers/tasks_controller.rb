46c3c261f712f8e515d56fff96e249c90ee0ed6f
Babajide Muritala <jide142000@yahoo.com>
2023-02-03 09:42:01 +0100

    updated design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
   def set_task
